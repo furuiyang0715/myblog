f1ffe0a58521dfa4c0c1ae8df5be3a2c9109d991
furuiyang <furuiyang0715@gmail.com>
2020-08-31 16:33:30 +0800

    配置环境变量

--- myblog.py
+++ myblog.py
@@ -14,3 +14,14 @@ if __name__ == '__main__':
 (2) # export FLASK_APP=microblog.py 
     # flask run
 '''
+
+
+'''
+在 flask 中优雅存储环境变量
+（1） 安装 python-dotenv
+（2） 在项目的根目录下新建一个名为 .flaskenv 的文件
+内容: 
+FLASK_APP=microblog.py
+ (3) 通过此配置, FLASK_APP 就可以自动加载 
+
+'''
