5365a0452d9a9ddb0c562cfdbdcbef8102f339ea
furuiyang <furuiyang0715@gmail.com>
2020-09-01 18:05:04 +0800

    用户头像服务

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+from hashlib import md5
 from datetime import datetime
 
 from flask_login import UserMixin
@@ -23,6 +24,10 @@ class User(UserMixin, db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest, size)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
