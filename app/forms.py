7f9551076b3d19054516d2632ba0073068ccc7a1
furuiyang <furuiyang0715@gmail.com>
2020-09-01 15:41:05 +0800

    注册的前端页面

--- app/forms.py
+++ app/forms.py
@@ -14,12 +14,15 @@ class LoginForm(FlaskForm):
 
 class RegistrationForm(FlaskForm):
     username = StringField('用户名', validators=[DataRequired()])
+    # Email， 这个来自WTForms的另一个验证器将确保用户在此字段中键入的内容与电子邮件地址的结构相匹配。
     email = StringField('邮箱', validators=[DataRequired(), Email()])
     password = PasswordField('密码', validators=[DataRequired()])
+    # EqualTo 的验证器，它将确保其值与第一个password字段的值相同。
     password2 = PasswordField('确认密码', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Register')
 
     def validate_username(self, username):
+        # 则通过ValidationError触发验证错误。 异常中作为参数的消息将会在对应字段旁边显示，以供用户查看。
         user = User.query.filter_by(username=username.data).first()
         if user is not None:
             raise ValidationError('Please use a different username.')
