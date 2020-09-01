a6a871c3739acf6e225c368a690a7afd04be7968
furuiyang <furuiyang0715@gmail.com>
2020-09-01 14:47:23 +0800

    汉化

--- app/forms.py
+++ app/forms.py
@@ -4,7 +4,7 @@ from wtforms.validators import DataRequired
 
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    remember_me = BooleanField('Remember Me')
-    submit = SubmitField('Sign In')
+    username = StringField('用户名', validators=[DataRequired()])
+    password = PasswordField('密码', validators=[DataRequired()])
+    remember_me = BooleanField('记住密码')
+    submit = SubmitField('登录')
