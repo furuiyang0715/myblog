adfc03d9dfe903476bbe7c6534ba9d8757037fbb
furuiyang <furuiyang0715@gmail.com>
2020-09-02 14:40:59 +0800

    添加电子邮件的日志实例对象

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,6 @@
+import logging
+from logging.handlers import SMTPHandler
+
 from flask import Flask
 from flask_login import LoginManager
 from flask_migrate import Migrate
@@ -22,6 +25,25 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 # 强制用户登录 跳转页面
 login.login_view = 'login'
+
+
+# 为 Flask 的日志对象 app.logger 添加了一个 SMTPHandler 的实例：
+if not app.debug:
+    if app.config['MAIL_SERVER']:
+        auth = None
+        if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+        secure = None
+        if app.config['MAIL_USE_TLS']:
+            secure = ()
+        mail_handler = SMTPHandler(
+            mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+            fromaddr='no-reply@' + app.config['MAIL_SERVER'],
+            toaddrs=app.config['ADMINS'], subject='我的微博发送信息失败',
+            credentials=auth, secure=secure,
+        )
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
 
 
 # 这样写是为了解决循环引用的问题
