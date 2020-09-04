9699f4a959f29aeb810ed071c63acedc549b3603
furuiyang <furuiyang0715@gmail.com>
2020-09-04 15:33:29 +0800

    使用多线程异步发送邮件

--- app/email.py
+++ app/email.py
@@ -1,14 +1,16 @@
+from threading import Thread
+
 from flask import render_template
 from flask_mail import Message
 
 from app import mail, app
 
 
-def send_email(subject, sender, recipients, text_body, html_body):
-    msg = Message(subject, sender=sender, recipients=recipients)
-    msg.body = text_body
-    msg.html = html_body
-    mail.send(msg)
+# def send_email(subject, sender, recipients, text_body, html_body):
+#     msg = Message(subject, sender=sender, recipients=recipients)
+#     msg.body = text_body
+#     msg.html = html_body
+#     mail.send(msg)
 
 
 def send_password_reset_email(user):
@@ -18,3 +20,16 @@ def send_password_reset_email(user):
                recipients=[user.email],
                text_body=render_template('email/reset_password.txt', user=user, token=token),
                html_body=render_template('email/reset_password.html', user=user, token=token))
+
+
+def send_async_email(app, msg):
+    with app.app_context():
+        mail.send(msg)
+
+
+def send_email(subject, sender, recipients, text_body, html_body):
+    """异步发送邮件"""
+    msg = Message(subject, sender=sender, recipients=recipients)
+    msg.body = text_body
+    msg.html = html_body
+    Thread(target=send_async_email, args=(app, msg)).start()
