8cc4b741516feb55edced39eb6a9dd4dde1bc075
furuiyang <furuiyang0715@gmail.com>
2020-09-04 11:38:22 +0800

    bug fixed

--- config.py
+++ config.py
@@ -9,27 +9,17 @@ class Config(object):
     # sqlite 数据库文件的路径
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # 发送邮件的配置
     MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp.163.com')
-    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
-    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_PORT = 465
+    MAIL_USE_SSL = True
+    MAIL_USE_TLS = False
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME', '15626046299@163.com')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD', os.environ.get("EMAIL_HOST_PASSWORD"))
-    ADMINS = [
-        '2564493603@qq.com',
-        '15626046299@163.com',
-    ]
+    ADMINS = ['15626046299@163.com']
 
     POSTS_PER_PAGE = 4  # 配置项 表示每页的配置列表长度
-
-    # EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    # EMAIL_HOST = 'smtp.163.com'
-    # EMAIL_PORT = 25
-    # # 发送邮件的邮箱
-    # EMAIL_HOST_USER = '15626046299@163.com'
-    # # 在邮箱中设置的客户端授权密码
-    # EMAIL_HOST_PASSWORD = configs.EMAIL_HOST_PASSWORD
-    # # 收件人看到的发件人
-    # EMAIL_FROM = 'mydailyfresh<15626046299@163.com>'
 
 
 # if __name__ == '__main__':
--- notes.py
+++ notes.py
@@ -6,6 +6,12 @@ pip install pyjwt
 （2） mail 插件化
 mail = Mail(app)
 
-
+(3) 进行配置之后 测试邮件的发送
+from flask_mail import Message
+from app import mail
+msg = Message('test subject', sender=app.config['ADMINS'][0], recipients=['2564493603@qq.com'])
+msg.body = 'text body'
+msg.html = '<h1>HTML body</h1>'
+mail.send(msg)
 
 '''
