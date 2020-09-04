e463de07d97996ddb488500f38ac0399982f7155
furuiyang <furuiyang0715@gmail.com>
2020-09-04 15:09:25 +0800

    Little

--- app/routes.py
+++ app/routes.py
@@ -262,8 +262,26 @@ def reset_password():
             user.password = password
             db.session.add(user)
             db.session.commit()
+            flash("密码重置成功")
             return redirect(url_for("login"))
         else:
             flash("当前链接已经过期")
             return redirect(url_for("login"))
     return render_template("reset_password.html", title='重置密码', form=form)
+
+
+# 作者的写法:
+# @app.route('/reset_password/<token>', methods=['GET', 'POST'])
+# def reset_password(token):
+#     if current_user.is_authenticated:
+#         return redirect(url_for('index'))
+#     user = User.verify_reset_password_token(token)
+#     if not user:
+#         return redirect(url_for('index'))
+#     form = ResetPasswordForm()
+#     if form.validate_on_submit():
+#         user.set_password(form.password.data)
+#         db.session.commit()
+#         flash('Your password has been reset.')
+#         return redirect(url_for('login'))
+#     return render_template('reset_password.html', form=form)
